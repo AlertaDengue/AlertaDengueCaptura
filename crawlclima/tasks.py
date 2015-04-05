68b9a5aaa7e6daf0d3911c68fe44288e35b83c73
Flavio C. Coelho <fccoelho@gmail.com>
2015-04-05 19:40:03 -0300

    Captura de uma estação funcionando.

--- crawlclima/tasks.py
+++ crawlclima/tasks.py
@@ -2,15 +2,16 @@ from __future__ import absolute_import
 
 from crawlclima.celery import app
 import requests
-from crawlclima import config
+from crawlclima.config import cemaden
 import pymongo
-
+from io import StringIO
+from datetime import datetime
 
 mongo = pymongo.MongoClient()
 
 
 @app.task
-def pega_dados_por_estacao(codigo, inicio, fim):
+def pega_dados_por_estacao(codigo, inicio, fim, recapture=False):
     """
     Esta tarefa captura dados climáticos de uma estação do CEMADEN, salvando os dados em um banco local.
     :param codigo: Código da estação de coleta do CEMADEN
@@ -18,11 +19,24 @@ def pega_dados_por_estacao(codigo, inicio, fim):
     :param fim: data-hora (UTC) de fim da captura %Y%m%d%H%M
     :return: Status code da
     """
+    col = mongo.clima.cemaden
+    if not recapture:
+        exists = col.find({"cod_estacao": codigo, "datahora": datetime.strptime(str(inicio), "%Y%m%d%H%M")}).count()
+        if exists:
+            return "skipping {}".format(inicio)
     #TODO: validar o código de estação
-    results = requests.get(config.cemaden.url_pcd, params={'chave': config.cemaden.chave,
+    results = requests.get(cemaden.url_pcd, params={'chave': cemaden.chave,
                                         'inicio': inicio,
                                         'fim': fim,
                                         'codigo': codigo})
+    fp = StringIO(results.text)
+    fp.readline()  # Remove o comentário
+    vnames = fp.readline().strip().split(';')
+    vnames = [v.replace('.', '_') for v in vnames]
+    for linha in fp:
+        doc = dict(zip(vnames, linha.strip().split(';')))
+        col.insert(doc)
+
     return results.status_code
 
 
