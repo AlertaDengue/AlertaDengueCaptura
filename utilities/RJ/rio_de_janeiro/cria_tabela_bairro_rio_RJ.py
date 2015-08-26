a82978b641da780a16f27d0dba724ad92d2519ce
Flavio C. Coelho <fccoelho@gmail.com>
2015-08-26 09:45:09 -0300

    identificadores com letras maiúsculas precisam ser envoltos em aspas.

--- utilities/RJ/rio_de_janeiro/cria_tabela_bairro_rio_RJ.py
+++ utilities/RJ/rio_de_janeiro/cria_tabela_bairro_rio_RJ.py
@@ -23,8 +23,8 @@ if __name__=="__main__":
     populações = pd.read_csv('popAPRJ.csv', header=0)
     bairrosAP = pd.read_csv("bairro2AP.csv", header=0)
     APS = populações.APS
-    loc_id = dict([(str(k),j) for j,k in APS.iteritems()])
-    sql = 'insert into "Municipio"."Bairro" (id, nome, "Localidade_id") values(%s,%s,%s);'
+    loc_id = dict([(str(k),int(j)) for j,k in APS.iteritems()])
+    sql = 'insert into "Municipio"."Bairro" (bairro_id, nome, "Localidade_id") values(%s,%s,%s);'
     for i, bairro in bairrosAP.iterrows():
         registro = (int(i), bairro.bairro, loc_id[str(bairro.APS)])
         cur.execute(sql, registro)
