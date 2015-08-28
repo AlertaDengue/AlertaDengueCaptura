4ddb83e276d33bcd38e966654a287e50b0eed25a
Flávio Codeço Coelho <fccoelho@gmail.com>
2015-08-28 17:45:12 -0300

    atualizei as tarefas do celerybeat para puxar das demais localidades

--- crawlclima/celeryconfig.py
+++ crawlclima/celeryconfig.py
@@ -24,11 +24,21 @@ CELERYBEAT_SCHEDULE = {
     'fetch-tweets-monday-morning': {
         'task': 'tasks.pega_tweets',
         'schedule': crontab(hour=9, minute=30, day_of_week=1),
-        'args': ((date.today()-timedelta(8)).isoformat(), date.today().isoformat(), [], "A90"),
+        'args': ((date.today()-timedelta(8)).isoformat(), date.today().isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90"),
     },
-    'fetch-cemaden-monday-morning':{
+    'fetch-cemaden-monday-morning-RJ': {
         'task': 'tasks.pega_dados_cemaden',
         'schedule': crontab(hour=8, minute=1, day_of_week=1),
         'args': ('RJ', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
+    },
+    'fetch-cemaden-monday-morning-MG': {
+        'task': 'tasks.pega_dados_cemaden',
+        'schedule': crontab(hour=8, minute=10, day_of_week=1),
+        'args': ('MG', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
+    },
+    'fetch-cemaden-monday-morning-PR': {
+        'task': 'tasks.pega_dados_cemaden',
+        'schedule': crontab(hour=8, minute=20, day_of_week=1),
+        'args': ('PR', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
     },
 }
