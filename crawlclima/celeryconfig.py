89d505d839e21e6b82c8e6dbae655b92d819b4b7
Flávio Codeço Coelho <fccoelho@gmail.com>
2015-08-31 09:00:16 -0300

    trying to fix celerybeat schedule

--- crawlclima/celeryconfig.py
+++ crawlclima/celeryconfig.py
@@ -26,23 +26,23 @@ CELERY_TIMEZONE = 'America/Sao_Paulo'
 CELERYBEAT_SCHEDULE = {
     # Executes every monday morning at 9:30 A.M
     'fetch-tweets-monday-morning': {
-        'task': 'tasks.pega_tweets',
+        'task': 'crawlclima.tasks.pega_tweets',
         'schedule': crontab(hour=9, minute=30, day_of_week=1),
-        'args': ((date.today()-timedelta(8)).isoformat(), date.today().isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90"),
+        'args': ((date.today()-timedelta(8)).isoformat(), date.today().isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90")
     },
     'fetch-cemaden-monday-morning-RJ': {
-        'task': 'tasks.pega_dados_cemaden',
-        'schedule': crontab(hour=8, minute=1, day_of_week=1),
-        'args': ('RJ', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
+        'task': 'crawlclima.tasks.pega_dados_cemaden',
+        'schedule': crontab(hour=9, minute=1, day_of_week=1),
+        'args': ('RJ', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal()))
     },
     'fetch-cemaden-monday-morning-MG': {
-        'task': 'tasks.pega_dados_cemaden',
-        'schedule': crontab(hour=8, minute=10, day_of_week=1),
-        'args': ('MG', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
+        'task': 'crawlclima.tasks.pega_dados_cemaden',
+        'schedule': crontab(hour=9, minute=10, day_of_week=1),
+        'args': ('MG', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal()))
     },
     'fetch-cemaden-monday-morning-PR': {
-        'task': 'tasks.pega_dados_cemaden',
-        'schedule': crontab(hour=8, minute=20, day_of_week=1),
-        'args': ('PR', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal())),
+        'task': 'crawlclima.tasks.pega_dados_cemaden',
+        'schedule': crontab(hour=9, minute=20, day_of_week=1),
+        'args': ('PR', datetime.fromordinal(date.today().toordinal())-timedelta(8), datetime.fromordinal(date.today().toordinal()))
     },
 }
