70e930635bafc705b1606d53a62687512cdaf096
Flávio Codeço Coelho <fccoelho@gmail.com>
2015-08-31 07:34:19 -0300

    Adiconei tabela comestimativas populacionais de 2014; e script para atualizar a população na tabela de município.

---
+++ utilities/update_mun_w_pop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Atualiza tabela de municípios com dados da população estimada
+"""
+import pandas as pd
+import psycopg2
+from crawlclima.config.general import psql_host, psql_user
+import logging
+
+logger = logging.getLogger("update-pop")
+
+try:
+    conn = psycopg2.connect("dbname='dengue' user='{}' host='{}' password='aldengue'".format(psql_user, psql_host))
+    cur = conn.cursor()
+except Exception as e:
+    logger.error("Unable to connect to Postgresql: {}".format(e))
+
+
+df = pd.read_csv("geocodigo_pop_estimada_2014.csv", header=0, sep=',')
+
+sql = 'update "Dengue_global"."Municipio" set populacao=%s WHERE geocodigo=%s;'
+for i, row in df.iterrows():
+    cur.execute(sql, (row.Pop_estimada, row.Codigo_Municipio))
+
+conn.commit()
+cur.close()
