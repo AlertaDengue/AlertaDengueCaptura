28611b6bca3685a0ebc10e153227260927ef2412
Flavio C. Coelho <fccoelho@gmail.com>
2015-10-15 15:54:56 -0300

    Adjusted scripts for cron execution

--- crawlclima/capturachuva.py
+++ crawlclima/capturachuva.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """
 Fetch a week of data from cemaden
+Once a week go over the entire year to fill in possible gaps in the local database
+requires celery worker to be up and running
+but this script will actually be executed by cron
 """
 
 from datetime import datetime, timedelta, date
@@ -10,8 +13,14 @@ from crawlclima.tasks import pega_dados_cemaden, mock
 
 today = datetime.fromordinal(date.today().toordinal())
 week_ago = datetime.fromordinal(date.today().toordinal())-timedelta(8)
+year_start = date(date.today().year, 1, 1)
 
-pega_dados_cemaden.delay('PR', week_ago, today, 'uf')
-pega_dados_cemaden.delay('RJ', week_ago, today, 'uf')
-pega_dados_cemaden.delay('MG', week_ago, today, 'uf')
+if today.isoweekday() != 5:
+    pega_dados_cemaden.delay('PR', week_ago, today, 'uf')
+    pega_dados_cemaden.delay('RJ', week_ago, today, 'uf')
+    pega_dados_cemaden.delay('MG', week_ago, today, 'uf')
+else:
+    pega_dados_cemaden.delay('PR', year_start, today, 'uf')
+    pega_dados_cemaden.delay('RJ', year_start, today, 'uf')
+    pega_dados_cemaden.delay('MG', year_start, today, 'uf')
 
--- crawlclima/pegatweets.py
+++ crawlclima/pegatweets.py
@@ -1,7 +1,9 @@
-
 #!/usr/bin/env python3
 """
 Fetch a week of tweets
+Once a week go over the entire year to fill in possible gaps in the local database
+requires celery worker to be up and running
+but this script will actually be executed by cron
 """
 
 from datetime import datetime, timedelta, date
@@ -11,6 +13,9 @@ from crawlclima.tasks import pega_tweets
 
 today = datetime.fromordinal(date.today().toordinal())
 week_ago = datetime.fromordinal(date.today().toordinal())-timedelta(8)
+year_start = date(date.today().year, 1, 1)
 
-pega_tweets.delay(week_ago.isoformat(), today.isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90")
-
+if today.isoweekday() == 5:
+    pega_tweets.delay(year_start.isoformat(), today.isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90")
+else:
+    pega_tweets.delay(week_ago.isoformat(), today.isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90")
