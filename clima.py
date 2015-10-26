cb29a128559610987c21a8985e15749b4364ffab
Israel Teixeira <israelst@gmail.com>
2015-10-26 13:49:02 -0200

    Cleanup clima.py.

--- clima.py
+++ clima.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 #coding:utf8
 """
-Este script captura series de clima de estações do Rio de Janeiro
-a partir do Weather underground
+Este script captura series de clima do servidor da Weather Underground em um
+período determinado.
+
 
 Copyright 2014 by Flávio Codeço Coelho
 license: GPL v3
 """
 import argparse
-import datetime
+from datetime import datetime
 
 from crawlclima.wunderground.wu import capture
 from utilities.models import save, find_all
@@ -17,15 +18,13 @@ from utilities.models import save, find_all
 rows = find_all(schema='Municipio', table='Estacao_wu')
 codes = [row['estacao_id'] for row in rows]
 
+date = lambda d: datetime.strptime(d, "%Y-%m-%d")
 
-parser = argparse.ArgumentParser(description="Pega séries de Clima do servidor da Weather Underground em um período determinado")
-parser.add_argument("--inicio", "-i", help="Data inicial de captura: yyyy-mm-dd")
-parser.add_argument("--fim", "-f", help="Data final de captura: yyyy-mm-dd")
-parser.add_argument("--codigo", "-c", choices=codes, help="Codigo da estação" )
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--inicio", "-i", type=date, help="Data inicial de captura: yyyy-mm-dd")
+parser.add_argument("--fim", "-f", type=date, help="Data final de captura: yyyy-mm-dd")
+parser.add_argument("--codigo", "-c", choices=codes, metavar='ICAO', help="Codigo da estação" )
 args = parser.parse_args()
-ini = datetime.datetime.strptime(args.inicio, "%Y-%m-%d")
-fim = datetime.datetime.strptime(args.fim, "%Y-%m-%d")
 
-data = capture(args.codigo, ini, fim)
+data = capture(args.codigo, args.inicio, args.fim)
 save(data, schema='Municipio', table='Clima_wu')
-
