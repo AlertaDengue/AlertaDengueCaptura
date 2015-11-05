cd794a5a9adaba00567598ba284aafd9d5a85a3f
Flavio C. Coelho <fccoelho@gmail.com>
2015-11-05 16:55:09 -0200

    fixed bug

--- crawlclima/pegatemperatura.py
+++ crawlclima/pegatemperatura.py
@@ -12,7 +12,7 @@ from utilities.models import save, find_all
 
 user_date = None
 if len(sys.argv) > 1:
-    user_date = datetime.strptime(sys.argv[1], format="%Y-%m-%d")
+    user_date = datetime.strptime(sys.argv[1], "%Y-%m-%d")
 
 today = datetime.today() if user_date is None else user_date
 year_start = datetime.today().year, 1, 1
