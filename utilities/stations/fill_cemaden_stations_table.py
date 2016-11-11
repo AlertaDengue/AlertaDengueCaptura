098f650242961d9874fddfedfe919c7d85540d45
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-11-11 13:41:29 -0200

    FInished script to load CEMADEN's stations metada to the database. Tested and working

--- utilities/stations/fill_cemaden_stations_table.py
+++ utilities/stations/fill_cemaden_stations_table.py
@@ -19,13 +19,63 @@ db_config = {
     'port': config('POSTGRES_PORT'),
 }
 
-base_url="http://150.163.255.246:18383/dados_rede"
+base_url="http://150.163.255.246:18383/dados_pcd"
 chave = "bc10602ea62759fab1578f8eb1ff6f7abbf8678d"
 
-def load_station_data(UF="RJ"):
+def get_connection():
+    try:
+        conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='aldengue'".format(db_config['database'], db_config['user'], 'localhost'))
+    except Exception as e:
+        print("Unable to connect to Postgresql: {}".format(e))
+        raise e
+    return conn
+
+def load_station_metadata(station_ID):
+    con = get_connection()
+    cur = con.cursor()
+    sql = 'insert INTO "Municipio"."Estacao_cemaden" (codestacao,nome,municipio,uf,latitude,longitude) values(%s, %s, %s, %s, %s, %s);'
     results = requests.get(base_url, params={'chave': chave,
-                                             'inicio': 201412010000,
-                                             'fim': 201412010300,
-                                             'uf': UF})
-    df = pd.read_csv(StringIO(results.text))
+                                             'inicio': 201610050000,
+                                             'fim': 201610051000,
+                                             'rede': 11,
+                                             'codigo': station_ID})
+    df = pd.read_csv(StringIO(results.text), skiprows=1, header=0, delimiter=';')
+    if df.size == 0:
+        print('Station {} returned no data'.format(station_ID))
+        return 1
+    # print(results.url)
+    print(df.ix[0])
+    record = {
+        'codestacao': str(station_ID),
+        'nome': df.ix[0]['nome'],
+        'municipio': df.ix[0]['municipio'],
+        'uf': df.ix[0]['uf'],
+        'latitude': float(df.ix[0]['latitude']),
+        'longitude': float(df.ix[0]['longitude'])
+    }
+    try:
+        cur.execute(sql, (record['codestacao'], record['nome'], record['municipio'], record['uf'], record['latitude'], record['longitude']))
+    except Exception as e:
+        print('Could not insert {}: {}'.format(station_ID, e))
+        # raise(e)
+    con.commit()
+    cur.close()
+
+def get_station_codes():
+    """
+    Get all CEMADEN station codes from database
+    :return: list of tuples (code, UF)
+    """
+    con = get_connection()
+    cur = con.cursor()
+    cur.execute('select distinct "Estacao_cemaden_codestacao" from "Municipio"."Clima_cemaden";')
+    codes = cur.fetchall()
+    cur.close()
+    return codes
+
+
+if __name__ == "__main__":
+    for code in get_station_codes():
+        load_station_metadata(code[0])
+        # break
 
