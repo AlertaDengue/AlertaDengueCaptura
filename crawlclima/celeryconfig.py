72127bd65fc32c7914bc5faa7359a37e815bb9a6
Flavio Amieiro <amieiro.flavio@gmail.com>
2017-11-22 19:58:43 +0100

    Cleans up celery config file

--- crawlclima/celeryconfig.py
+++ crawlclima/celeryconfig.py
@@ -1,59 +1,24 @@
-from celery.schedules import crontab
-from datetime import datetime, timedelta, date
 from decouple import config
 
-## Broker settings.
+# Broker settings.
 BROKER_URL = 'amqp://'
 
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ('crawlclima.tasks', )
 
-## Using the database to store task state and results.
+# Using the database to store task state and results.
 CELERY_RESULT_BACKEND = 'rpc://'
-#CELERY_TASK_RESULT_EXPIRES = 18000  # 5 hours.
+# CELERY_TASK_RESULT_EXPIRES = 18000  # 5 hours.
 
 # CELERY_ROUTES = {
 #     'crawlclima.tasks.pega_dados_cemaden': 'high-priority',
 # }
 
-CELERY_ANNOTATIONS = {'crawlclima.tasks.pega_dados_cemaden': {'rate_limit': '10/s'},
-                      'crawlclima.tasks.fetch_wunderground': {'rate_limit': '1/s'}
-                      }
-
-#Celery beat configurations
+CELERY_ANNOTATIONS = {
+    'crawlclima.tasks.pega_dados_cemaden': {'rate_limit': '10/s'},
+    'crawlclima.tasks.fetch_wunderground': {'rate_limit': '1/s'}
+}
 
 CELERY_TIMEZONE = 'America/Sao_Paulo'
 
 CELERY_ALWAYS_EAGER = config('CELERY_ALWAYS_EAGER', default=False)
-
-# today = datetime.fromordinal(date.today().toordinal())
-# week_ago = datetime.fromordinal(date.today().toordinal())-timedelta(8)
-
-# CELERYBEAT_SCHEDULE = {
-#     # Executes every monday morning at 9:30 A.M
-#     'fetch-tweets-monday-morning': {
-#         'task': 'crawlclima.tasks.pega_tweets',
-#         'schedule': crontab(hour=9, minute=30, day_of_week=1),
-#         'args': ((date.today()-timedelta(8)).isoformat(), date.today().isoformat(), ['3304557', '3303302', '3106200', '4104808'], "A90")
-#     },
-#     'fetch-cemaden-monday-morning-RJ': {
-#         'task': 'crawlclima.tasks.pega_dados_cemaden',
-#         'schedule': crontab(hour=8, minute=1, day_of_week=1),
-#         'args': ('RJ', week_ago, today, 'uf')
-#     },
-#     'fetch-cemaden-monday-morning-MG': {
-#         'task': 'crawlclima.tasks.pega_dados_cemaden',
-#         'schedule': crontab(hour=8, minute=1, day_of_week=1),
-#         'args': ('MG', week_ago, today, 'uf')
-#     },
-#     'fetch-cemaden-monday-morning-PR': {
-#         'task': 'crawlclima.tasks.pega_dados_cemaden',
-#         'schedule': crontab(hour=8, minute=1, day_of_week=1),
-#         'args': ('PR', week_ago, today, 'uf')
-#     },
-#     'fetch-wunderground-daily': {
-#         'task': 'crawlclima.tasks.fetch_wunderground_all',
-#         'schedule': crontab(hour=9, minute=0),
-#         'args': (today, )
-#     },
-# }
