59e5125548117280130759e895d1f78b8a343d6c
felipebottega <felipebottega@gmail.com>
2019-07-02 17:42:42 -0300

    Minor improvements

--- downloader_app/tiff_downloader.py
+++ downloader_app/tiff_downloader.py
@@ -1,3 +1,4 @@
+from configparser import ConfigParser
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,9 +11,20 @@ from rasterio.warp import reproject, Resampling
 import xarray as xr
 import geoviews as gv
 from celery import Celery
+from celery.utils.log import get_task_logger
 
 
-def download_tiffs(source, date1, date2, point1, point2, path, opt=False):
+logger = get_task_logger("downloader_app")
+
+# instantiate
+config = ConfigParser()
+# parse existing file
+config.read('settings.ini')
+# read values 
+PATH = config.get('settings', 'path')
+
+
+def download_tiffs(source, date1, date2, point1, point2, opt=False):
     """ Function responsible for the main calls. 
     
         Inputs
@@ -28,8 +40,6 @@ def download_tiffs(source, date1, date2, point1, point2, path, opt=False):
         point1, point2: tuple
             point1 is a tuple (x, y) corresponding to the upper left point of the image, 
             while point2 corresponds to the lower right point.
-        path: string
-            The path where the downloaded files will be stored.
         opt: classe
             All possible extra options are passed as variables of this class. If no class 
             is passed, the default(False) is assumed, which means that no extra options 
@@ -52,15 +62,13 @@ def download_tiffs(source, date1, date2, point1, point2, path, opt=False):
 
     # Dates of interest.
     dates = pd.date_range(year1 + month1 + day1, year2 + month2 + day2, freq=freq)
-    # print(dates)
-    # print()
-    
+        
     # Download maps by date.
     length = len(dates)
     for l in range(length-1):
         current_date = dates[l]
         next_date = dates[l+1]
-        url = single_download(source, current_date, next_date, x1, x2, y1, y2, path, opt)
+        url = single_download(source, current_date, next_date, x1, x2, y1, y2, opt)
                         
     # View time series after the downloads.
     if opt.time_series:
@@ -69,7 +77,7 @@ def download_tiffs(source, date1, date2, point1, point2, path, opt=False):
     return 
 
 
-def single_download(source, date1, date2, x1, x2, y1, y2, path, opt):
+def single_download(source, date1, date2, x1, x2, y1, y2, opt):
     """ Function responsible for each satellite image download. """
 
     # Convert numeric data values to string format. 
@@ -99,59 +107,62 @@ def single_download(source, date1, date2, x1, x2, y1, y2, path, opt):
     url_base = start_url + time_range + bounding_box + time + end_url
     url = url_base.format(month1, day1, day2)
 
-    # Generate filename.
+    # Generate filename.    
     filename = str(year1) + '-' + str(month1) + '-' + str(day1) + '.tiff'
+    path = os.path.join(PATH, filename)
 
     # Remove duplicate file with same name if it exists.
-    exists = os.path.isfile(path + "/" + filename)
+    exists = os.path.isfile(path)
     if exists:
-        os.remove(path + "/" + filename)
+        os.remove(path)
 
     # Download and save url content.
     try:
         # Download.
         response = urllib.request.urlopen(url)
         data = response.read()
-        with open(path + "/" + filename, 'wb') as file:
+        with open(path, 'wb') as file:
             file.write(data)
         file.close()
-        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):success'
-        print(msg)
         
         # Fix values scale (the images are downloaded as uint8, not float).
         url_dods = start_url + time_range + bounding_box + time + 'dods'
         remote_data = xr.open_dataset(url_dods, decode_times=False)
         a = remote_data['LST'].scale_min
         b = remote_data['LST'].scale_max
-        with rasterio.open(path + "/" + filename) as dataset:
+        with rasterio.open(path) as dataset:
             profile = dataset.profile
             profile.update(dtype=rasterio.float32)
             array = dataset.read()
-            #m = np.min(array[0, :, :])
-            #M = np.max(array[0, :, :])
             m = 0
             M = 255
         new_array = (b-a)/(M-m)*array[0, :, :] + a - m*(b-a)/(M-m)
         new_array = np.array(new_array, dtype=np.float32)
-        with rasterio.open(path + "/" + filename, 'w', **profile) as dataset:
+        with rasterio.open(path, 'w', **profile) as dataset:
             dataset.write(new_array, 1)        
         
         # After saving the image, the treatment process begins.
-        regrid_image(filename, path, opt)
+        regrid_image(filename, opt)
+        
+        # Log message of success.
+        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):success'
+        logger.info(msg)
         
     except urllib.error.HTTPError:
-        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):fail'
-        print(msg)
+        # Log message of failure.
+        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):failure'
+        logger.error(msg)
         
     except urllib.error.URLError:
-        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):fail'
-        print(msg)
+        # Log message of failure.
+        msg = 'Download (' + year1 + '-' + month1 + '-' + day1 + ') (' + year2 + '-' + month2 + '-' + day2 + '):failure'
+        logger.error(msg)
         
     # If the treated image is the only one required, the original image is deleted.
     if not opt.keep_original:
-        exists = os.path.isfile(path + "/" + filename)
+        exists = os.path.isfile(path)
         if exists:
-            os.remove(path + "/" + filename)
+            os.remove(path)
     
     return url
 
@@ -337,14 +348,16 @@ def about(x):
     return
 
 
-def regrid_image(filename, path, opt):
+def regrid_image(filename, opt):
     """ This function is responsible for upsampling or downsampling the images using some precribed method. """
+    
+    path = os.path.join(PATH, filename)
     
     if opt.regrid:
         factor = opt.regrid[0]
         method = opt.regrid[1]
 
-        with rasterio.open(path + "/" + filename) as dataset:
+        with rasterio.open(path) as dataset:
             a = dataset.transform.a
             b = dataset.transform.b
             c = dataset.transform.c
@@ -355,8 +368,6 @@ def regrid_image(filename, path, opt):
             new_array = np.ones((int(factor*array.shape[1]), int(factor*array.shape[2])), dtype=np.float32)
             new_transform = (a/factor, b, c, d, e/factor, f)
             dataset_crs = dataset.crs        
-            #dataset_crs = {'init': 'EPSG:4326'}
-            #new_crs = {'init': 'EPSG:4326'}
             new_crs = dataset.crs
         
             if method == 'nearest':
@@ -415,15 +426,16 @@ def regrid_image(filename, path, opt):
 
         # Remove duplicates before saving new ones.
         new_filename = 'new_' + filename
-        exists = os.path.isfile(path + "/" + new_filename)
+        new_path = os.path.join(PATH, new_filename)
+        exists = os.path.isfile(new_path)
         if exists:
-            os.remove(path + "/" + new_filename)
+            os.remove(new_path)
         
-        with rasterio.open(path + "/" + new_filename, 'w', **new_profile) as new_dataset:
+        with rasterio.open(new_path, 'w', **new_profile) as new_dataset:
             new_dataset.write_band(1, new_array)
             
     else:
-        with rasterio.open(path + "/" + filename) as dataset:
+        with rasterio.open(path) as dataset:
             array = dataset.read()
         if opt.plot:
             plt.imshow(array[0, :, :], cmap=opt.cmap)
