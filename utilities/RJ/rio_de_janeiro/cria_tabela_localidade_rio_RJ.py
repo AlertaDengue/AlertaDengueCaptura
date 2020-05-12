f17e636807aeac06d8882c68b2a58e3cac42d6cb
Sandro Loch <es.loch@gmail.com>
2020-05-12 12:20:27 -0300

    Fixes undefined name 'config'

--- utilities/RJ/rio_de_janeiro/cria_tabela_localidade_rio_RJ.py
+++ utilities/RJ/rio_de_janeiro/cria_tabela_localidade_rio_RJ.py
@@ -1,18 +1,27 @@
+import logging
+import sys
+
+import geojson
 import pandas as pd
 import psycopg2
-import geojson
-import sys
-import logging
+from decouple import config
 
 logger = logging.getLogger("crialoc")
 
 try:
-    conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(
-        config('POSTGRES_DATABASE'), config('POSTGRES_USER'), config('POSTGRES_HOST'), config('POSTGRES_PASSWORD')))
+    conn = psycopg2.connect(
+        "dbname='{}' user='{}' host='{}' password='{}'".format(
+            config("POSTGRES_DATABASE"),
+            config("POSTGRES_USER"),
+            config("POSTGRES_HOST"),
+            config("POSTGRES_PASSWORD"),
+        )
+    )
     cur = conn.cursor()
 except Exception as e:
     logger.error("Unable to connect to Postgresql: {}".format(e))
     sys.exit()
+
 
 def pega_poligono_AP(ap, mapa):
     """
@@ -34,14 +43,19 @@ def pega_poligono_AP(ap, mapa):
     return pol
 
 
-
-if __name__=="__main__":
-    populações = pd.read_csv('popAPRJ.csv', header=0)
+if __name__ == "__main__":
+    populações = pd.read_csv("popAPRJ.csv", header=0)
     APS = populações.APS
     sql = 'insert into "Municipio"."Localidade" (id,nome,populacao,geojson,"Municipio_geocodigo") values(%s,%s,%s,%s,%s);'
     for i, ap in populações.iterrows():
         pol = pega_poligono_AP(str(ap.APS), "cap_sms.geojson")
-        registro = (int(i), pol['properties']['NOME'], float(ap.Pop2010), geojson.dumps(pol), 3304557)
+        registro = (
+            int(i),
+            pol["properties"]["NOME"],
+            float(ap.Pop2010),
+            geojson.dumps(pol),
+            3304557,
+        )
         cur.execute(sql, registro)
     conn.commit()
     cur.close
