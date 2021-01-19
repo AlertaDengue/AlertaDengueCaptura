8d712307407553cdf0b72cc1b2918b1643dc334b
esloch <es.loch@gmail.com>
2021-01-19 12:09:24 -0300

    Modify celery app and celeryconfig file from downloader

--- downloader_app/celeryapp.py
+++ downloader_app/celeryapp.py
@@ -1,18 +1,8 @@
-import os
+# Create app celery to start Downloader_app
 
 from celery import Celery
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-# Create the app and set the broker location (RabbitMQ).
-app = Celery(
-    "downloader_app",
-    broker=os.getenv("CELERY_BROKER_URL"),
-    backend=os.getenv("CELERY_BACKEND"),  #
-    include=["downloader_app.tasks"],
-)
+app = Celery("downloader_app",)
 
 app.config_from_object("downloader_app.celeryconfig")
 
--- downloader_app/celeryconfig.py
+++ downloader_app/celeryconfig.py
@@ -1,16 +1,16 @@
+# Parameters are used in the celery app
+# Optional configuration, see the application user guide.
+
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Broker settings.
-BROKER_URL = os.getenv("CELERY_BROKER_URL")
-# List of modules to import when celery starts.
-CELERY_IMPORTS = ("downloader_app.tasks",)
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
-CELERYD_MAX_TASKS_PER_CHILD = 10
-CELERY_TIMEZONE = "America/Sao_Paulo"
-CELERY_ALWAYS_EAGER = os.getenv("CELERY_ALWAYS_EAGER", default=False)
+broker_url = os.getenv("CELERY_BROKER_URL")
+include = ("downloader_app.tasks",)
+accept_content = ['application/json']
+result_serializer = 'json'
+worker_max_tasks_per_child = 10
+timezone = "America/Sao_Paulo"
+task_always_eager = False
