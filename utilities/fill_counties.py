6542bfbbf0bab58dce33dcac31a6c8b56a0a654e
esloch <es.loch@gmail.com>
2021-02-12 13:57:07 -0300

    Use 'else' conditional statement to API

--- utilities/fill_counties.py
+++ utilities/fill_counties.py
@@ -2,15 +2,15 @@ import csv
 import functools
 import os
 import time
-import warnings
 from multiprocessing.pool import Pool
 from os.path import abspath, dirname
 from os.path import join as join_path
 
 import geojson
 import requests
-from initials import initials
-from models import save
+
+from utilities.initials import initials
+from utilities.models import counties_save
 
 
 # Get geojson from geocode
@@ -39,37 +39,37 @@ def uf_geojson(uf):
     return geojson.load(open(join_path(path, filename), "r"))
 
 
+# TODO: We should improve the complexity of this function
 def county_polygon(uf, county_code):
-    if uf == 0:
+    for feature in uf_geojson(uf)["features"]:
+        if feature["properties"].get("CD_GEOCODM") == county_code:
+            return geojson.dumps(feature)
+    else:
+        print(
+            f"{county_code} is not in this geojson {uf}, redirecting the capture to API..."
+        )
         for feature in read_json(county_code)['features']:
             if feature["properties"].get("codarea") == county_code:
                 return geojson.dumps(feature)
-    else:
-        for feature in uf_geojson(uf)["features"]:
-            if feature["properties"].get("CD_GEOCODM") == county_code:
-                return geojson.dumps(feature)
-    warnings.warn("is not in this geojson: {}.".format(county_code))
-    return
 
 
 def to_row(county):
     county_code = county["Cod Municipio Completo"]
     name = county["Nome_Município"]
     uf = county["Nome_UF"]
-
     geojson = county_polygon(initials[uf], county_code)
-    print(county_code, name, uf)
+    print(county_code, "|", name, "|", uf)
 
     return dict(
         county_code=county_code,
         name=name,
         geojson=geojson,
-        uf=initials[uf],
+        uf=uf,
         population=0,
     )
 
 
 BASE_DIR = dirname(abspath(__file__))
-path = join_path(BASE_DIR, "DTB_Only_Municipio_faltantes.csv")
+path = join_path(BASE_DIR, "DTB_2014_Municipio.csv")
 rows = Pool().map(to_row, csv.DictReader(open(path)))
-save(rows, schema="Dengue_global", table="Municipio")
+counties_save(rows, schema="Dengue_global", table="Municipio")
