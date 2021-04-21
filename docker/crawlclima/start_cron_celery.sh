ff773d1379d658b27aec3b6bbfeb0447016f10c7
esloch <es.loch@gmail.com>
2021-04-21 07:08:14 -0300

    Change script to start cron

--- docker/crawlclima/start_cron_celery.sh
+++ docker/crawlclima/start_cron_celery.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Executing the script!"
+echo "Installing Python Packages..."
 python docker/crawlclima/setup.py develop
 sleep 10
+echo "Force crond start"
+service cron restart
+echo "Start celery workert"
 exec celery -A crawlclima.fetchapp worker -l info --concurrency=4
-cron && tail -f /var/log/cron.log
