b6b5985b7c83f82025c311d9114b4d52c0ee328c
esloch <es.loch@gmail.com>
2022-12-01 16:24:20 -0300

    Remove apps

--- downloader/docker/satellite/start_cron_celery.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python docker/satellite/setup.py develop
-sleep 10
-exec celery -A downloader_app.celeryapp worker -l info --concurrency=4
-touch /var/log/cron.log
-cron && tail -f /var/log/cron.log
